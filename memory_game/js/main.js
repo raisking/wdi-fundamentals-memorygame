314ce4a1275c3eb6f1bacf00e70acba91dcd94f5
Prakash Rai <raiprakash@Prakashs-MacBook-Pro.local>
2017-08-27 21:28:51 -0400

    update logic game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,39 +1,78 @@
-var cards = ["queen", "queen", "king" , "king"];
+// var cards = ["queen", "queen", "king" , "king"];
+var cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png'
+	},
+	{
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: 'images/queen-of-diamonds.png'
+	},
+		{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: 'images/king-of-hearts.png'
+	},
+		{
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: 'images/king-of-diamonds.png'
+	}
+];
+
 var cardsInPlay = [];
 
 function checkForMatch(){
-
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match!");
+      alert("You found a match!");
   } else {
-      console.log("Sorry, try again.");
+      alert("Sorry, try again.");
   }
 }
-checkForMatch();//call from inside flipCard fun
-function flipCard(cardId){
 
-	// var cardOne = cards[0];
-	// cardsInPlay.push(cardOne);
-	//  console.log("User flipped " + cardOne);
-
-	// var cardTwo = cards[2]; 
-	// cardsInPlay.push(cardTwo);
-	// console.log("User flipped " + cardTwo);
-
-	cardsInPlay.push(cards[cardId]);
+function flipCard() {
+	cardId = this.getAttribute('data-id');
+	console.log(cardId);
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 
 	if (cardsInPlay.length === 2){
-	 	if (cardsInPlay[0]===cardsInPlay[2]){
-	 		alert("You found a match!");
-	 	}else{
-	 		alert("Sorry, Try again");
-	 	}
+		checkForMatch();
+		cardsInPlay = [];
 	}
 
-	console.log("User flipped" + cards[cardId]);
+
+};
+
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+
+  }
 }
-flipCard(0);
-flipCard(2);
-//exercise 9
+createBoard();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// start exercise 10
+
 
 	
